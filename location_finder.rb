a32355f9a06dccde5f55d7f2b1f4ea853d42cb8a
Sergio Robaudo <srobaudo@cipherhealth.com>
2018-02-02 12:05:26 -0300

    get rid of unneeded instance generation

--- location_finder.rb
+++ location_finder.rb
@@ -14,21 +14,9 @@ class LocationFinder
     end
 
     def instance_methods_locations(target)
-      instance = try_get_instance(target)
-      if instance
-        target.instance_methods.map{ |method|
-          instance.method(method).source_location
-        }.compact.map{ |s| s.first }.uniq
-      end
-    end
-
-    def try_get_instance(target, param_count: 0)
-      params = Array.new(param_count, '')
-      target.new(*params)
-    rescue ArgumentError => e
-      try_get_instance(target, param_count: param_count + 1)
-    rescue => e
-      nil
+      target.instance_methods.map{ |method|
+        target.instance_method(method).source_location
+      }.compact.map{ |s| s.first }.uniq
     end
   end
 end
